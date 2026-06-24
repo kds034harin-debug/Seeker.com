63a997fb8fd574e523b7fb21e669d795f6cbf65f
kds034harin-debug <kds034harin@icloud.com>
2026-06-24 07:56:22 +0300

    data.js

--- data.js
+++ data.js
@@ -60,49 +60,49 @@ let contentDatabase = [
         links: { watch: "https://www.kinopoisk.ru/series/the-last-of-us-2023/" }
     },
 
-    // ========== ИГРЫ (С РАБОЧИМИ КАРТИНКАМИ) ==========
-{ 
-    id: 9001, type: "game", title: "Ведьмак", year: "2007", rating: "8.5",
-    description: "Первая часть культовой RPG-серии.",
-    image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7u.jpg",
-    developer: "CD Projekt RED", platforms: "PC", genre: "RPG, Action",
-    links: { buy: "https://store.steampowered.com/app/20900/" }
-},
-{ 
-    id: 9002, type: "game", title: "Ведьмак 2: Убийцы королей", year: "2011", rating: "8.9",
-    description: "Продолжение приключений Геральта.",
-    image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7v.jpg",
-    developer: "CD Projekt RED", platforms: "PC, Xbox 360", genre: "RPG, Action",
-    links: { buy: "https://store.steampowered.com/app/20920/" }
-},
-{ 
-    id: 9, type: "game", title: "Ведьмак 3: Дикая Охота", year: "2015", rating: "9.6",
-    description: "Легендарная RPG от CD Projekt RED.",
-    image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7w.jpg",
-    developer: "CD Projekt RED", platforms: "PC, PS5, Xbox, Switch", genre: "RPG, Action",
-    links: { buy: "https://store.steampowered.com/app/292030/" }
-},
-{ 
-    id: 10, type: "game", title: "Cyberpunk 2077", year: "2020", rating: "8.2",
-    description: "Приключения наёмника Ви в Найт-Сити.",
-    image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7x.jpg",
-    developer: "CD Projekt RED", platforms: "PC, PS5, Xbox", genre: "Киберпанк, RPG",
-    links: { buy: "https://store.steampowered.com/app/1091500/" }
-},
-{ 
-    id: 11, type: "game", title: "The Last of Us Part I", year: "2022", rating: "9.5",
-    description: "Ремастер культовой игры о выживании.",
-    image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7y.jpg",
-    developer: "Naughty Dog", platforms: "PC, PS5", genre: "Action-adventure",
-    links: { buy: "https://store.steampowered.com/app/1888930/" }
-},
-{ 
-    id: 9003, type: "game", title: "The Last of Us Part II", year: "2020", rating: "9.3",
-    description: "Продолжение культовой игры.",
-    image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7z.jpg",
-    developer: "Naughty Dog", platforms: "PS4, PS5, PC", genre: "Action-adventure",
-    links: { buy: "https://store.playstation.com/ru-ru/product/UP9000-CUSA07820_00-THELASTOFUS2DIG0" }
-}
+    // ========== ИГРЫ ==========
+    { 
+        id: 9001, type: "game", title: "Ведьмак", year: "2007", rating: "8.5",
+        description: "Первая часть культовой RPG-серии. Геральт из Ривии отправляется на поиски похищенной возлюбленной.",
+        image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7u.jpg",
+        developer: "CD Projekt RED", platforms: "PC", genre: "RPG, Action",
+        links: { buy: "https://store.steampowered.com/app/20900/The_Witcher_Enhanced_Edition_Directors_Cut/" }
+    },
+    { 
+        id: 9002, type: "game", title: "Ведьмак 2: Убийцы королей", year: "2011", rating: "8.9",
+        description: "Продолжение приключений Геральта. Сюжет разворачивается вокруг убийства короля и политических интриг.",
+        image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7v.jpg",
+        developer: "CD Projekt RED", platforms: "PC, Xbox 360", genre: "RPG, Action",
+        links: { buy: "https://store.steampowered.com/app/20920/The_Witcher_2_Assassins_of_Kings_Enhanced_Edition/" }
+    },
+    { 
+        id: 9, type: "game", title: "Ведьмак 3: Дикая Охота", year: "2015", rating: "9.6",
+        description: "Легендарная RPG от CD Projekt RED. Завершающая часть трилогии о Геральте из Ривии.",
+        image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7w.jpg",
+        developer: "CD Projekt RED", platforms: "PC, PS5, Xbox, Switch", genre: "RPG, Action",
+        links: { buy: "https://store.steampowered.com/app/292030/The_Witcher_3_Wild_Hunt/" }
+    },
+    { 
+        id: 10, type: "game", title: "Cyberpunk 2077", year: "2020", rating: "8.2",
+        description: "Приключения наёмника Ви в Найт-Сити. Сюжетная RPG в мире киберпанка от создателей Ведьмака.",
+        image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7x.jpg",
+        developer: "CD Projekt RED", platforms: "PC, PS5, Xbox", genre: "Киберпанк, RPG",
+        links: { buy: "https://store.steampowered.com/app/1091500/Cyberpunk_2077/" }
+    },
+    { 
+        id: 11, type: "game", title: "The Last of Us Part I", year: "2022", rating: "9.5",
+        description: "Ремастер культовой игры о выживании в постапокалиптическом мире.",
+        image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7y.jpg",
+        developer: "Naughty Dog", platforms: "PC, PS5", genre: "Action-adventure",
+        links: { buy: "https://store.steampowered.com/app/1888930/The_Last_of_Us_Part_I/" }
+    },
+    { 
+        id: 9003, type: "game", title: "The Last of Us Part II", year: "2020", rating: "9.3",
+        description: "Продолжение культовой игры. История мести и прощения.",
+        image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7z.jpg",
+        developer: "Naughty Dog", platforms: "PS4, PS5, PC", genre: "Action-adventure",
+        links: { buy: "https://store.playstation.com/ru-ru/product/UP9000-CUSA07820_00-THELASTOFUS2DIG0" }
+    },
 
     // ========== КНИГИ ==========
     { 
